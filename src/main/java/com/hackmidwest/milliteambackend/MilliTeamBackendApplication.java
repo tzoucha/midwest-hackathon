df6746325060de3d48b5453e77f5163910aa8225
Evan Stohlmann <evans@gen6ventures.com>
2022-07-23 12:08:41 -0500

    initial spring commit

---
+++ src/main/java/com/hackmidwest/milliteambackend/MilliTeamBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hackmidwest.milliteambackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilliTeamBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilliTeamBackendApplication.class, args);
+	}
+
+}
