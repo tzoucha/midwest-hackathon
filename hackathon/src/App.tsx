1d27e997991491b812942ec03fbef8b249623181
Jonathan Guerrero <jonathang@gen6ventures.com>
2022-07-23 13:16:50 -0500

    Start creating frontend services

--- hackathon/src/App.tsx
+++ hackathon/src/App.tsx
@@ -33,44 +33,63 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import { useServices } from './services/providers';
+import { useEffect, useState } from 'react';
 
 setupIonicReact();
+
+const AuthChooser = () => {
+  const services = useServices();
+  const [isAuthorized, setIsAuthorized] = useState(false)
+  useEffect(() => {
+    (async () => {
+      await services.authService.initialize();
+      setIsAuthorized(services.authService.isAuthorized())
+    })()
+  }, [services.authService]);
+  return (isAuthorized
+    ? <AuthedRoutes />
+    : <input type='button' value='LOGIN' onClick={() => setIsAuthorized(true)}/>)
+}
 
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
-      <IonTabs>
-        <IonRouterOutlet>
-          <Route exact path="/tab1">
-            <Tab1 />
-          </Route>
-          <Route exact path="/tab2">
-            <Tab2 />
-          </Route>
-          <Route path="/tab3">
-            <Tab3 />
-          </Route>
-          <Route exact path="/">
-            <Redirect to="/tab1" />
-          </Route>
-        </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
+      <AuthChooser />
     </IonReactRouter>
   </IonApp>
 );
 
 export default App;
+const AuthedRoutes = () => 
+  <IonTabs>
+    <IonRouterOutlet>
+      <Route exact path="/tab1">
+        <Tab1 />
+      </Route>
+      <Route exact path="/tab2">
+        <Tab2 />
+      </Route>
+      <Route path="/tab3">
+        <Tab3 />
+      </Route>
+      <Route exact path="/">
+        <Redirect to="/tab1" />
+      </Route>
+    </IonRouterOutlet>
+    <IonTabBar slot="bottom">
+      <IonTabButton tab="tab1" href="/tab1">
+        <IonIcon icon={triangle} />
+        <IonLabel>Tab 1</IonLabel>
+      </IonTabButton>
+      <IonTabButton tab="tab2" href="/tab2">
+        <IonIcon icon={ellipse} />
+        <IonLabel>Tab 2</IonLabel>
+      </IonTabButton>
+      <IonTabButton tab="tab3" href="/tab3">
+        <IonIcon icon={square} />
+        <IonLabel>Tab 3</IonLabel>
+      </IonTabButton>
+    </IonTabBar>
+  </IonTabs>;
+
