f9150d87c47940f464e3f540a12ce74918415760
Evan Stohlmann <evans@gen6ventures.com>
2022-07-23 13:02:30 -0500

    initial mongo accessing

---
+++ src/main/java/com/hackmidwest/milliteambackend/config/MongoConfig.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright(c)2001-2020 First National Bank of Omaha 1620 Dodge Street Omaha, NE 68197, USA All
+ * rights reserved.
+ *
+ * <p>This software is the confidential and proprietary information of First National Bank
+ * ("Confidential Information"). You shall not disclose such Confidential Information and shall use
+ * it only in accordance with the terms of the license agreement you entered into with First
+ * National Bank
+ */
+package com.hackmidwest.milliteambackend.config;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@Configuration
+public class MongoConfig {
+  @Bean
+  public MongoClient mongo() {
+    ConnectionString connectionString = new ConnectionString("mongodb+srv://milli-heroku:milli2022@cluster0.oxqds9m.mongodb.net/?retryWrites=true&w=majority");
+    MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+        .applyConnectionString(connectionString)
+        .build();
+
+    return MongoClients.create(mongoClientSettings);
+  }
+
+  @Bean
+  public MongoTemplate mongoTemplate() throws Exception {
+    return new MongoTemplate(mongo(), "test");
+  }
+}
---
+++ src/main/java/com/hackmidwest/milliteambackend/model/Customer.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright(c)2001-2020 First National Bank of Omaha 1620 Dodge Street Omaha, NE 68197, USA All
+ * rights reserved.
+ *
+ * <p>This software is the confidential and proprietary information of First National Bank
+ * ("Confidential Information"). You shall not disclose such Confidential Information and shall use
+ * it only in accordance with the terms of the license agreement you entered into with First
+ * National Bank
+ */
+package com.hackmidwest.milliteambackend.model;
+
+import org.springframework.data.annotation.Id;
+
+public class Customer {
+
+  @Id
+  public String id;
+  public String firstName;
+  public String lastName;
+  public String hashedPassword;
+  public String profilePicture;
+  public String emailAddress;
+  public String addressLine1;
+  public String city;
+  public String state;
+  public String zipCode;
+
+  public String getId() {
+    return id;
+  }
+
+  public Customer setId(String id) {
+    this.id = id;
+    return this;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public Customer setFirstName(String firstName) {
+    this.firstName = firstName;
+    return this;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public Customer setLastName(String lastName) {
+    this.lastName = lastName;
+    return this;
+  }
+
+  public String getHashedPassword() {
+    return hashedPassword;
+  }
+
+  public Customer setHashedPassword(String hashedPassword) {
+    this.hashedPassword = hashedPassword;
+    return this;
+  }
+
+  public String getProfilePicture() {
+    return profilePicture;
+  }
+
+  public Customer setProfilePicture(String profilePicture) {
+    this.profilePicture = profilePicture;
+    return this;
+  }
+
+  public String getEmailAddress() {
+    return emailAddress;
+  }
+
+  public Customer setEmailAddress(String emailAddress) {
+    this.emailAddress = emailAddress;
+    return this;
+  }
+
+  public String getAddressLine1() {
+    return addressLine1;
+  }
+
+  public Customer setAddressLine1(String addressLine1) {
+    this.addressLine1 = addressLine1;
+    return this;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public Customer setCity(String city) {
+    this.city = city;
+    return this;
+  }
+
+  public String getState() {
+    return state;
+  }
+
+  public Customer setState(String state) {
+    this.state = state;
+    return this;
+  }
+
+  public String getZipCode() {
+    return zipCode;
+  }
+
+  public Customer setZipCode(String zipCode) {
+    this.zipCode = zipCode;
+    return this;
+  }
+
+  @Override
+  public String toString() {
+    return "Customer{" +
+        "id='" + id + '\'' +
+        ", firstName='" + firstName + '\'' +
+        ", lastName='" + lastName + '\'' +
+        ", hashedPassword='" + hashedPassword + '\'' +
+        ", profilePicture='" + profilePicture + '\'' +
+        ", emailAddress='" + emailAddress + '\'' +
+        ", addressLine1='" + addressLine1 + '\'' +
+        ", city='" + city + '\'' +
+        ", state='" + state + '\'' +
+        ", zipCode='" + zipCode + '\'' +
+        '}';
+  }
+}
