33660222389c6e709654d0440346b00b7bb51f81
Evan Stohlmann <evans@gen6ventures.com>
2022-07-23 21:41:16 -0500

    test data updates

--- src/main/java/com/hackmidwest/milliteambackend/model/PinataUploadResponse.java
+++ src/main/java/com/hackmidwest/milliteambackend/model/PinataUploadResponse.java
@@ -5,6 +5,7 @@ import java.util.Date;
 public class PinataUploadResponse {
   public int status;
   public ArrayList<Item> items;
+  public Item item;
   public static class Item {
     public String id;
     public Date createdAt;
@@ -16,6 +17,98 @@ public class PinataUploadResponse {
     public String type;
     public boolean pinToIPFS;
     public String uri;
+
+    public String getId() {
+      return id;
+    }
+
+    public Item setId(String id) {
+      this.id = id;
+      return this;
+    }
+
+    public Date getCreatedAt() {
+      return createdAt;
+    }
+
+    public Item setCreatedAt(Date createdAt) {
+      this.createdAt = createdAt;
+      return this;
+    }
+
+    public String getCid() {
+      return cid;
+    }
+
+    public Item setCid(String cid) {
+      this.cid = cid;
+      return this;
+    }
+
+    public String getName() {
+      return name;
+    }
+
+    public Item setName(String name) {
+      this.name = name;
+      return this;
+    }
+
+    public String getOriginalname() {
+      return originalname;
+    }
+
+    public Item setOriginalname(String originalname) {
+      this.originalname = originalname;
+      return this;
+    }
+
+    public String getSize() {
+      return size;
+    }
+
+    public Item setSize(String size) {
+      this.size = size;
+      return this;
+    }
+
+    public Metadata getMetadata() {
+      return metadata;
+    }
+
+    public Item setMetadata(
+        Metadata metadata) {
+      this.metadata = metadata;
+      return this;
+    }
+
+    public String getType() {
+      return type;
+    }
+
+    public Item setType(String type) {
+      this.type = type;
+      return this;
+    }
+
+    public boolean isPinToIPFS() {
+      return pinToIPFS;
+    }
+
+    public Item setPinToIPFS(boolean pinToIPFS) {
+      this.pinToIPFS = pinToIPFS;
+      return this;
+    }
+
+    public String getUri() {
+      return uri;
+    }
+
+    public Item setUri(String uri) {
+      this.uri = uri;
+      return this;
+    }
+
     public static class KeyValues {
       public String example;
     }
--- src/main/java/com/hackmidwest/milliteambackend/service/PinataService.java
+++ src/main/java/com/hackmidwest/milliteambackend/service/PinataService.java
@@ -1,5 +1,11 @@
 package com.hackmidwest.milliteambackend.service;
 
+import com.hackmidwest.milliteambackend.config.PinataConfig;
+import com.hackmidwest.milliteambackend.model.PinataUploadResponse;
+import com.hackmidwest.milliteambackend.repo.CustomerRepository;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -8,15 +14,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
-
-import com.hackmidwest.milliteambackend.config.PinataConfig;
-import com.hackmidwest.milliteambackend.model.PinataUploadResponse;
-import com.hackmidwest.milliteambackend.repo.CustomerRepository;
-
-import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
-@Slf4j
 @Service
 public class PinataService {
   @Autowired
@@ -32,7 +31,22 @@ public class PinataService {
       .baseUrl("https://managed.mypinata.cloud/api/v1/").build();
 
   public Mono<byte[]> getPinataNFT(String id) {
-    return milliGateway.get().uri(id)
+    Map<String,Object> body = new HashMap<>();
+    body.put("timeoutSeconds", 3600);
+    body.put("contentIds", Collections.singletonList(id));
+
+    String resp = submarineApi.post().uri("/auth/content/jwt")
+        .header("x-api-key", config.getApiKey())
+        .header("Content-Type", "application/json")
+        .body(BodyInserters.fromValue(body)).retrieve().bodyToMono(String.class).doOnError(r -> r.printStackTrace()).block();
+
+    PinataUploadResponse content  = submarineApi.get().uri("/content/" + id)
+        .header("x-api-key", config.getApiKey())
+        .header("Content-Type", "application/json")
+        .retrieve().bodyToMono(PinataUploadResponse.class).doOnError(r -> r.printStackTrace()).block();
+
+
+    return milliGateway.get().uri(content.item.getCid() + "?accessToken=" + resp.replaceAll("\"", ""))
         // .header("x-api-key", config.getApiKey())
         .retrieve()
         .bodyToMono(byte[].class);
@@ -54,37 +68,20 @@ public class PinataService {
         .body(BodyInserters.fromMultipartData(builder.build()))
         .exchangeToMono(response -> {
           if (response.statusCode().equals(HttpStatus.OK)) {
-          
             return response.bodyToMono(PinataUploadResponse.class);
           } else {
-            log.error("Error uploading data", response.bodyToMono(String.class));
+            System.out.println("Error uploading data " + response.bodyToMono(String.class));
             throw new RuntimeException("Error uploading");
             // throw new RuntimeException("Error uploading file" +
             // response.bodyToMono(String.class).block());
           }
         }).doOnSuccess(result -> {
           repo.findById(userId).ifPresent(customer -> {
-            customer.setProfilePicture(result.items.get(0).cid);
+            customer.setProfilePicture(result.items.get(0).id);
             repo.save(customer);
           });
         }).block();
 
   }
 
-  // data.append('files',
-  // fs.createReadStream('/Users/battousai/repos/midwest-hackathon/hackathon/public/profile_pic.jpeg'));
-  // data.append('name', 'My File');
-  // data.append('metadata', '{"keyvalues": { "example": "value" }}');
-  // data.append('wrapWithDirectory', 'false');
-  // data.append('pinToIPFS', 'false');
-
-  // var config = {
-  // method: 'post',
-  // url: 'https://managed.mypinata.cloud/api/v1/content',
-  // headers: {
-  // 'x-api-key': 'j0o7Iq0m4KWbK2b5co1igo3iOfNC4ZSS',
-  // ...data.getHeaders()
-  // },
-  // data : data
-  // };
 }
