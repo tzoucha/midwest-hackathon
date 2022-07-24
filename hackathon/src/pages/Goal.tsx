bbad40c6a0f4d4134b6f173dbf5e67a09582f49a
Damen Abbott <damen@gen6ventures.com>
2022-07-24 07:10:01 -0500

    resize icon

--- hackathon/src/pages/Goal.tsx
+++ hackathon/src/pages/Goal.tsx
@@ -64,7 +64,7 @@ const Goal: React.FC = () => {
         </IonHeader>
         <IonGrid>
           <IonRow>
-            <IonCol size='12' style={{ textAlign: 'center' }}>
+            <IonCol size='12' style={{ textAlign: 'center', padding: '0px 75px' }}>
               <IonAvatar style={{ width: 'auto', height: 'auto' }}>
                 <img style={{ border: `6px ${(pocketInfo.data.color || 'black')} solid` }} src={`${baseUrl}/profile-pic/${pocketInfo.data.picture}`} />
                 {/* <IonButton color='light' style={{position: 'absolute', top: 0, right: 0}} onClick={() => filePicker.current?.click()}><IonIcon icon={createOutline} size='large' /></IonButton> */}
